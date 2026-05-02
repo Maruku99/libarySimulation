d89d355a22b5f979e818fe7d946436430c004bbc
marc wendt <mail@marcwendt.com>
2026-05-02 12:22:29 +0200

    initial commit testing setup

---
+++ src/main/java/com/marcwendt/libarySim/libarySimApplication.java
@@ -0,0 +1,27 @@
+package com.marcwendt.libarySim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class libarySimApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(libarySimApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(/*@RequestParam(value = "name", defaultValue = "World") String name*/) {
+		// return String.format("Hello %s! neues Buch: [%s]", name);
+		return "Hello World";
+	}
+
+	@GetMapping("/book")
+	public String book(){
+		Book book = new Book(0, 1235664, "Harry Otter", "der Stein der alte", true);
+		return book.printBook(book);
+	}
+}
