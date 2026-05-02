ee7c109b8bf12013e47f09352e6481e6374bc7f1
marc wendt <mail@marcwendt.com>
2026-05-02 21:07:09 +0200

    Ändere die Methode helloBooks in getAllBooks, um eine Liste von Büchern zurückzugeben

--- src/main/java/com/marcwendt/libarySim/controller/BookController.java
+++ src/main/java/com/marcwendt/libarySim/controller/BookController.java
@@ -1,13 +1,22 @@
 package com.marcwendt.libarySim.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcwendt.libarySim.model.Book;
 
 @RestController
 public class BookController {
     @GetMapping("/books")
-    public String helloBooks() {
-		return String.format("Hello books");
-	}
+    public List<Book> getAllBooks() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1, "Clean Code", "Robert C. Martin", true));
+        books.add(new Book(2, "Der Hobbit", "J.R.R. Tolkien", true));
+        books.add(new Book(3, "1984", "George Orwell", false));
+        return books;
+    }
 
 }
