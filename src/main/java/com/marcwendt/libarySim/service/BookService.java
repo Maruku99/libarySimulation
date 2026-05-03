4374d403198a36da5e640e8f07dc413522eeb021
marc wendt <mail@marcwendt.com>
2026-05-03 12:41:24 +0200

    Implementiere BookService mit CRUD-Methoden für Bücher

--- src/main/java/com/marcwendt/libarySim/service/BookService.java
+++ src/main/java/com/marcwendt/libarySim/service/BookService.java
@@ -1,8 +1,38 @@
 package com.marcwendt.libarySim.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.marcwendt.libarySim.model.Book;
+import com.marcwendt.libarySim.repository.BookRepository;
 
 @Service
 public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // Alle Bücher zurückgeben
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    // Einzelnes Buch suchen
+    public Book getBookById(long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Buch nicht gefunden"));
+    }
+
+    // Buch hinzufügen
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Buch löschen
+    public void deleteBook(long id) {
+        bookRepository.deleteById(id);
+    }
 
 }
