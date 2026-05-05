ef43d4640e267fbf8a8042da9806871d18ce7d1b
marc wendt <mail@marcwendt.com>
2026-05-05 10:59:13 +0200

    Füge Methode findByIsAvailable hinzu: Ermögliche das Abrufen von Büchern basierend auf Verfügbarkeit

--- src/main/java/com/marcwendt/libarySim/repository/BookRepository.java
+++ src/main/java/com/marcwendt/libarySim/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.marcwendt.libarySim.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.marcwendt.libarySim.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    // leer lassen – Spring macht den Rest
+    List<Book> findByIsAvailable(boolean isAvailable);
 }
